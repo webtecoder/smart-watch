a2e37fc93bd7812ef31335b9e3103d65999e0a38
sazzadhossen360 <sazzadhossen2580@gmail.com>
2021-04-23 21:45:53 +0600

    added off canvas overlay

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
 
 $('.slider').slick({
